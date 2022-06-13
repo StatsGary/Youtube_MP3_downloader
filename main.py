18a2c83624ddc658cad4c822ac14c5f440e278ed
Gary Hutson <g.hutson@nhs.net>
2022-06-13 16:50:11 +0100

    Added changes to function

--- main.py
+++ main.py
@@ -1,15 +1,32 @@
 from youtubedownloader.getyoutubemp3 import get_youtube_mp3
 import pandas as pd
 from tqdm import tqdm
+import os.path
+import os
+import glob
+
+def download_multi_mp3s(download_df, url_field, output_dir='Downloads', verbose=True):
+    if os.path.exists('Downloads'):
+        files = glob.glob('Downloads/*')
+        for f in files:
+            os.remove(f)
+    
+    downloads = pd.read_csv(download_df)
+    url_list = downloads[url_field]
+    for idx, link in enumerate(tqdm(url_list)):
+        if verbose:
+            print(f'\n[INFO] downloading {link} at index {idx + 1}.')
+        # Run the function to get our MP3 files    
+        get_youtube_mp3(link, output_dir=output_dir)
+    return url_list
+
 
 if __name__== '__main__':
-    # Create a function to read a CSV full of Youtube links and then use
-    # function to download them one by one
-    downloads = pd.read_csv('YoutubeDownloads.csv')
-    # Use the URL and convert to list
-    url_list = list(downloads.YoutubeLink)
+    # Use our download function
+    download_list = download_multi_mp3s(
+        download_df='YoutubeDownloads.csv',
+        url_field='YoutubeLink',
+        output_dir='Downloads',
+        verbose=False)
 
-    for idx, link in enumerate(tqdm(url_list)):
-        print(f'[INFO] downloading {link} at index {idx + 1}.')
-        get_youtube_mp3(link, output_dir='Downloads')
-    
+    print(download_list)
